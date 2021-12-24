2c1e3fced4bf53aae35b6a233bc1abc991c6ddcf
GabrieleRusso11 <93495918+GabrieleRusso11@users.noreply.github.com>
2021-12-24 12:06:43 +0100

    Add files via upload

--- producer_shm.c
+++ producer_shm.c
@@ -1,65 +1,73 @@
-/* 
- * ARP - Advanced Robot Programming
- * Assignment 2 - Producer / Consumer data transfer
- * Authors: Simone Contorno and Grabiele Russo
- */
-
-// Use: ./socket hostname port max_size
-
-// Headers
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
-
 #include <stdlib.h>
-#include <strings.h>
 #include <string.h>
-#include <unistd.h>
-
-#include <time.h>
-#include <sys/wait.h> 
-#include <fcntl.h> 
-#include <sys/stat.h>
-#include <math.h>
-
+#include <fcntl.h>
 #include <sys/shm.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
+#include <semaphore.h>
 
-void error(char *msg) {
-    perror(msg);
-    exit(-1);
-}
+int main(int argc, char * argv[]){
 
-int main (int argc, char* argv[]) {
-    const char *shm_name = "/CB";
-    const int SIZE = 3;
-    int i, shm_fd;
+    const char * shm_name = "/CircularBuffer";
+    const int SIZE = 4096; // is a default value, after we redefine the size using ftruncate
+    const char * message[] = {"Il","Prof","Renato","Zaccaria","Non","Spiega","un","Cazzo","Di","Niente","Porco","ZIOOOOOO","!!"};
+    int shm_fd;
     void *ptr;
 
+    int dim = 5;
+    sem_t full;
+    sem_t empty;
+    sem_t mutex;
+
+    sem_init(&empty, 7, dim); // the second parameter specifies whether or not the initialized semaphore is shared between processes.
+                              // A non zero value means the semaphore is shared between processes and a value of zero means it is shared between threads.
+    sem_init(&full, 7, 0);
+    sem_init(&mutex, 7, 1);
+
+    int in = 0;
+    int out = 0;
+    int CB[dim];
+
     shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == 1) 
-        error("ERROR making shared memory segment");
-
-    const char *message[] = {"1","2","3","4","5","6","7","8","9","0","1","2","3","4","5","6","7","8","9","0"};
-
-    ftruncate(shm_fd, sizeof(message));
-    ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (ptr == MAP_FAILED) 
-        error("ERROR making mapping");
-    void *temp_ptr = ptr;
-    for (i = 0; i < sizeof(message); ++i) {
-        if (i == SIZE)
-            ptr = temp_ptr;
-        sprintf(ptr, "%s", message[i]);
-        //printf("Pointer: %s\n", (char *) ptr);
+    if(shm_fd == -1){
+        printf("shm_open error : shared memory segment failed\n");
         fflush(stdout);
-        ptr += strlen(message[i]);
+        exit(1);
     }
 
-    munmap(ptr, SIZE);
-    shm_unlink(shm_name);
+    if(ftruncate(shm_fd,dim) == -1){
+        printf("ftruncate error\n");
+        fflush(stdout);
+        exit(1);
+    }
+    ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    if(ptr == MAP_FAILED){
+        printf("Map failed\n");
+        fflush(stdout);
+        exit(1);
+    } 
+    
+
+    /*write into the memory segment*/
+    for(int i = 0; i < strlen(*message); i++){
+        
+        sem_wait(&empty); //it waits when there are no empty slots
+        sem_wait(&mutex);
+        sprintf(ptr, "%s", message[i]);
+        printf("the Producer insert the word %s at %i\n", buffer[in], in);
+        in = (in+1) % dim;
+        ptr += strlen(message[i]);
+        sem_post(&mutex);
+        sem_post(&full); // It says to the consumer that the buffer has some data and it can consume now
+    }
+    munmap(ptr,SIZE);
+    shm_unlink("/CircularBuffer");
+
+    sem_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
 
     return 0;
 }
